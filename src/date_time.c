954379a6497e6afd253ee840062f1f54252def53
Ismail Yakini <ismailyakini5@gmail.com>
2023-01-01 10:55:17 +0100

    Solving the problem of the year doesn't change

--- src/date_time.c
+++ src/date_time.c
@@ -6,8 +6,9 @@ char buffer[100];
 void print_date(){
 	now = time(NULL);
 	cur_time = localtime(&now);
-	strftime(buffer, 100, "%A %d %B %G", cur_time);	
+	strftime(buffer, 100, "%A %d %B ", cur_time);	
 	printf("%s", buffer);
+	printf("%d", cur_time->tm_year + 1900);
 }
 
 // This function displays the time on the screen
